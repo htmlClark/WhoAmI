ace376cf458280e6e5ab7d01e5ffb9a1bc87667c
Lily's one and only Clarkie <sadiconairon4@gmail.com>
2026-04-11 14:34:16 +0800

    Colored flags
    - Added color flags for text outputs
    
    - Removed N/A
    
    - No Fixes

--- UILayer/Program.cs
+++ UILayer/Program.cs
@@ -1,22 +1,40 @@
-﻿using LogicLayer; 
+﻿using System.Reflection.Metadata;
+using LogicLayer; 
 
 namespace UI
 {
 
     // WhoAmI Console App version : 0.1.0 - Initial Project Setup 
-    // WhoamI Console App version : 0.2.1 - (Feature) Live database (Sqlite Only) | (Fix) Name validation preveneting emptry whitespaces to bypass the read. 
+    // WhoAmI Console App version : 0.2.1 - (Feature) Live database (Sqlite Only) | (Fix) Name validation preveneting emptry whitespaces to bypass the read.
+    // WhoAmI Console App version : 0.3.1 - (Feature) Color coded text outputs | (Fix) None
     class Program
     {
         static void Main()
         {
             AppService appService = new AppService();
 
-            if(appService.dbInit()) 
+            const string ASCII = """ 
+            ========================================
+
+                    STUDENT ENROLLMENT SYSTEM       
+                        
+            ========================================
+            """;
+
+            if (appService.dbInit())
             {
-                Console.WriteLine("--- Connection secured! ---\n"); 
+                Console.WriteLine(ASCII);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Status: Connected\n");
+                Console.ResetColor();
                 App(appService);
             }
-            else Console.Write("--- Failed to connect... ---");
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Status: Disconnected\n");
+                Console.ResetColor();
+            }
   
         }
 
@@ -26,13 +44,14 @@ namespace UI
 
             while(isRunning)
             {
-               
-                Console.Write("Enter your name (type \"X\" to exit): ");
+                Console.Write("> Enter your name (X to exit): ");
                 string Name = Console.ReadLine();
 
                 if(string.IsNullOrWhiteSpace(Name)) 
                 {
-                    Console.WriteLine("Name cannot be empty.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("- Name cannot be empty.\n");
+                    Console.ResetColor();
                     continue;
                 }
 
@@ -40,16 +59,23 @@ namespace UI
 
                 if(Name.ToLower() == "x")
                 {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("- Closing Program...");
+                    Console.ResetColor();
                     break;
                 }
 
                 if (appService.isNameValid(Name))
                 {
-                    Console.WriteLine($"Hello, {Name}!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"- Hello, {Name}!\n");
+                    Console.ResetColor();
                 }
                 else
                 {
-                    Console.WriteLine($" \"{Name}\" is not an enrolled student.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"- \"{Name}\" is not an enrolled student.\n");
+                    Console.ResetColor();
                 }
             }
         }
